a88b67fe0da0dba92123886989f40f5b5a27f03f
Hector <hector@hector.com>
2023-02-22 03:33:33 -0600

    vagrant added

--- script.sh
+++ script.sh
@@ -9,6 +9,9 @@ echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-arptables && \
   echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-ip6tables && \
   echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-iptables
 nomad -version
-sudo apt-get install -y linux-modules-extra-$(uname -r)
-sudo nomad agent -dev -bind 0.0.0.0 -log-level INFO
 !nomad node status
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vagrant
+#sudo apt-get install -y linux-modules-extra-$(uname -r)
+#sudo nomad agent -dev -bind 0.0.0.0 -log-level INFO
