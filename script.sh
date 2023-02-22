e139e448f055dfce50d43deb1b2c343224b4048d
Hector <hector@hector.com>
2023-02-22 01:42:35 -0600

    primer setup infra

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install nomad
+curl -L -o cni-plugins.tgz "https://github.com/containernetworking/plugins/releases/download/v1.0.0/cni-plugins-linux-$( [ $(uname -m) = aarch64 ] && echo arm64 || echo amd64)"-v1.0.0.tgz && \
+  sudo mkdir -p /opt/cni/bin && \
+  sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
+echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-arptables && \
+  echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-ip6tables && \
+  echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-iptables
+  nomad -version
